a887e4f2644c76f5852290f3157208cfd5c7ff53
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-05-08 13:08:38 +0200

    started the GUI with tkinter

---
+++ frontend_GUI.py
@@ -0,0 +1,9 @@
+'''
+This is going to be the graphical user interface for the interactive dictionary - developed with tkinter
+'''
+
+from tkinter import *
+
+window = Tk(className='Interactive Dictionary')
+
+window.mainloop()
